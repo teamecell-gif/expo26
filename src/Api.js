aea433b5d9e29d7aea007aa0d8fd97832865ea79
AryanSinghGangwar <aryansingh81104@gmail.com>
2026-02-04 02:32:26 +0530

    Added backend

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "http://localhost:5000" // Ensure this matches your backend PORT
+  baseURL: "https://expo-backend-wv2k.onrender.com" // Ensure this matches your backend PORT
 });
 
 export const registerUser = async (formData) => {
